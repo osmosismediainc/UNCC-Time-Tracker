94124b407abf2b0d21897f349eee72d1d0890b44
Denise Middleton <demiddleton@bellsouth.net>
2019-11-03 13:32:32 -0500

    created code to grab form values to addEmployee

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
   });
   $(".modal-trigger").leanModal();
   $(".carousel").carousel();
+  //Add New Employee
+  $("#addEmployee-btn").on("click", function() {
+    event.preventDefault();
+
+    // Grabbed Values
+    var tName = $("#lastName" + ", " + "#firstName");
+    var tUserName = $("#userName")
+      .val()
+      .trim();
+    var tPassword = $("#password")
+      .val()
+      .trim();
+    //Check variables
+    console.log(tName);
+    console.log(tUserName);
+    console.log(tPassword);
+    //Create new row to add new employee data
+    var newRow = $("<tr>");
+    newRow.prepend($("<td>" + tName + "</td>"));
+    newRow.prepend($("<td>" + tUserName + "</td>"));
+    newRow.prepend($("<td>" + tPassword + "</td>"));
+    // Prepend the new row to the table
+    $("#employeeList").prepend(newRow);
+  });
 });
