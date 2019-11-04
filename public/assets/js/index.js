89954907e05c8cf28deadc4d2ce5132630cbd30d
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-11-04 00:15:12 -0500

    testing

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -11,4 +11,46 @@ $(document).ready(function() {
   });
   $(".modal-trigger").leanModal();
   $(".carousel").carousel();
+
+  var currentUser = {
+    id: 0,
+    userId: "",
+    empName: "",
+    manager: false
+  };
+
+  // Detects if the users userName and Password are in the database. If not throw err
+  $("#login-btn").on("click", function() {
+    var userIdInput = $("#userIdInput").val();
+    var loginPassword = $("#loginPassword").val();
+    console.log(userIdInput);
+    console.log(loginPassword);
+    var queryUrl = "/api/view-employees/" + userIdInput + "/" + loginPassword;
+
+    if (userIdInput === "" || loginPassword === "") {
+      console.log("Err");
+      return false;
+    } else {
+      $.get(queryUrl, function(data) {
+        if (data) {
+          console.log(data);
+          currentUser = {
+            id: data.id,
+            userId: data.userId,
+            empName: data.empName,
+            manager: data.manager
+          };
+          $($("#login-btn")).attr("href", "/home");
+          console.log(currentUser);
+        } else {
+          console.log("Error");
+          return false;
+        }
+      });
+    }
+  });
+
+  $("#clockIn").on("click", function() {
+    console.log(currentUser);
+  });
 });
